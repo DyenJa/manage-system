0d88d15196c407d5669460873e6172ad3781c4cb
dyenja <aprilat@163.com>
2020-02-07 19:36:12 +0800

    Initial commit

---
+++ src/test/java/com/odm/managesystem/ManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odm.managesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
