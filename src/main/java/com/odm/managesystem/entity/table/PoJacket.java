ca5c7e8fff1f1f5dedbaf691b3f56ccce7d31211
dyenja <aprilat@163.com>
2020-02-12 18:07:20 +0800

    modify some tables, and made some entity classes. Finished a Mapper and its XML file's writing.

---
+++ src/main/java/com/odm/managesystem/entity/table/PoJacket.java
@@ -0,0 +1,347 @@
+package com.odm.managesystem.entity.table;
+
+
+public class PoJacket {
+
+  private String id;
+  private long directOrdered;
+  private String name;
+  private String keyWord1;
+  private String keyWord2;
+  private String keyWord3;
+  private String productGroup;
+  private String brand;
+  private String placket;
+  private String length;
+  private String collar;
+  private String decration;
+  private String detachPart;
+  private String fabricType;
+  private String feature;
+  private String paddingMaterial;
+  private String lining;
+  private String type;
+  private String patternType;
+  private String producePlace;
+  private String process;
+  private String season;
+  private String surfaceMaterial;
+  private String sleeveLength;
+  private String sleeveType;
+  private String style;
+  private String supplyType;
+  private String craft;
+  private String thickness;
+  private String weight;
+  private String productType;
+  private String customizeFeature;
+  private String color;
+  private String size;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public long getDirectOrdered() {
+    return directOrdered;
+  }
+
+  public void setDirectOrdered(long directOrdered) {
+    this.directOrdered = directOrdered;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getKeyWord1() {
+    return keyWord1;
+  }
+
+  public void setKeyWord1(String keyWord1) {
+    this.keyWord1 = keyWord1;
+  }
+
+
+  public String getKeyWord2() {
+    return keyWord2;
+  }
+
+  public void setKeyWord2(String keyWord2) {
+    this.keyWord2 = keyWord2;
+  }
+
+
+  public String getKeyWord3() {
+    return keyWord3;
+  }
+
+  public void setKeyWord3(String keyWord3) {
+    this.keyWord3 = keyWord3;
+  }
+
+
+  public String getProductGroup() {
+    return productGroup;
+  }
+
+  public void setProductGroup(String productGroup) {
+    this.productGroup = productGroup;
+  }
+
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+
+  public String getPlacket() {
+    return placket;
+  }
+
+  public void setPlacket(String placket) {
+    this.placket = placket;
+  }
+
+
+  public String getLength() {
+    return length;
+  }
+
+  public void setLength(String length) {
+    this.length = length;
+  }
+
+
+  public String getCollar() {
+    return collar;
+  }
+
+  public void setCollar(String collar) {
+    this.collar = collar;
+  }
+
+
+  public String getDecration() {
+    return decration;
+  }
+
+  public void setDecration(String decration) {
+    this.decration = decration;
+  }
+
+
+  public String getDetachPart() {
+    return detachPart;
+  }
+
+  public void setDetachPart(String detachPart) {
+    this.detachPart = detachPart;
+  }
+
+
+  public String getFabricType() {
+    return fabricType;
+  }
+
+  public void setFabricType(String fabricType) {
+    this.fabricType = fabricType;
+  }
+
+
+  public String getFeature() {
+    return feature;
+  }
+
+  public void setFeature(String feature) {
+    this.feature = feature;
+  }
+
+
+  public String getPaddingMaterial() {
+    return paddingMaterial;
+  }
+
+  public void setPaddingMaterial(String paddingMaterial) {
+    this.paddingMaterial = paddingMaterial;
+  }
+
+
+  public String getLining() {
+    return lining;
+  }
+
+  public void setLining(String lining) {
+    this.lining = lining;
+  }
+
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+  public String getPatternType() {
+    return patternType;
+  }
+
+  public void setPatternType(String patternType) {
+    this.patternType = patternType;
+  }
+
+
+  public String getProducePlace() {
+    return producePlace;
+  }
+
+  public void setProducePlace(String producePlace) {
+    this.producePlace = producePlace;
+  }
+
+
+  public String getProcess() {
+    return process;
+  }
+
+  public void setProcess(String process) {
+    this.process = process;
+  }
+
+
+  public String getSeason() {
+    return season;
+  }
+
+  public void setSeason(String season) {
+    this.season = season;
+  }
+
+
+  public String getSurfaceMaterial() {
+    return surfaceMaterial;
+  }
+
+  public void setSurfaceMaterial(String surfaceMaterial) {
+    this.surfaceMaterial = surfaceMaterial;
+  }
+
+
+  public String getSleeveLength() {
+    return sleeveLength;
+  }
+
+  public void setSleeveLength(String sleeveLength) {
+    this.sleeveLength = sleeveLength;
+  }
+
+
+  public String getSleeveType() {
+    return sleeveType;
+  }
+
+  public void setSleeveType(String sleeveType) {
+    this.sleeveType = sleeveType;
+  }
+
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+
+  public String getSupplyType() {
+    return supplyType;
+  }
+
+  public void setSupplyType(String supplyType) {
+    this.supplyType = supplyType;
+  }
+
+
+  public String getCraft() {
+    return craft;
+  }
+
+  public void setCraft(String craft) {
+    this.craft = craft;
+  }
+
+
+  public String getThickness() {
+    return thickness;
+  }
+
+  public void setThickness(String thickness) {
+    this.thickness = thickness;
+  }
+
+
+  public String getWeight() {
+    return weight;
+  }
+
+  public void setWeight(String weight) {
+    this.weight = weight;
+  }
+
+
+  public String getProductType() {
+    return productType;
+  }
+
+  public void setProductType(String productType) {
+    this.productType = productType;
+  }
+
+
+  public String getCustomizeFeature() {
+    return customizeFeature;
+  }
+
+  public void setCustomizeFeature(String customizeFeature) {
+    this.customizeFeature = customizeFeature;
+  }
+
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+
+}
---
+++ src/main/java/com/odm/managesystem/entity/table/PoTshirt.java
@@ -0,0 +1,277 @@
+package com.odm.managesystem.entity.table;
+
+
+public class PoTshirt {
+
+  private String id;
+  private long directOrdered;
+  private String name;
+  private String keyWord1;
+  private String keyWord2;
+  private String keyWord3;
+  private String productGroup;
+  private long avaliableInventoryNow;
+  private String avaliableSizeNow;
+  private String brand;
+  private String length;
+  private String collar;
+  private String decration;
+  private String fabricType;
+  private String fabricWeight;
+  private String feature;
+  private String material;
+  private String type;
+  private String patternType;
+  private String producePlace;
+  private String sleeveLength;
+  private String sleeveStyle;
+  private String fengge;
+  private String craft;
+  private String customizeFeature;
+  private String color;
+  private String size;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public long getDirectOrdered() {
+    return directOrdered;
+  }
+
+  public void setDirectOrdered(long directOrdered) {
+    this.directOrdered = directOrdered;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getKeyWord1() {
+    return keyWord1;
+  }
+
+  public void setKeyWord1(String keyWord1) {
+    this.keyWord1 = keyWord1;
+  }
+
+
+  public String getKeyWord2() {
+    return keyWord2;
+  }
+
+  public void setKeyWord2(String keyWord2) {
+    this.keyWord2 = keyWord2;
+  }
+
+
+  public String getKeyWord3() {
+    return keyWord3;
+  }
+
+  public void setKeyWord3(String keyWord3) {
+    this.keyWord3 = keyWord3;
+  }
+
+
+  public String getProductGroup() {
+    return productGroup;
+  }
+
+  public void setProductGroup(String productGroup) {
+    this.productGroup = productGroup;
+  }
+
+
+  public long getAvaliableInventoryNow() {
+    return avaliableInventoryNow;
+  }
+
+  public void setAvaliableInventoryNow(long avaliableInventoryNow) {
+    this.avaliableInventoryNow = avaliableInventoryNow;
+  }
+
+
+  public String getAvaliableSizeNow() {
+    return avaliableSizeNow;
+  }
+
+  public void setAvaliableSizeNow(String avaliableSizeNow) {
+    this.avaliableSizeNow = avaliableSizeNow;
+  }
+
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+
+  public String getLength() {
+    return length;
+  }
+
+  public void setLength(String length) {
+    this.length = length;
+  }
+
+
+  public String getCollar() {
+    return collar;
+  }
+
+  public void setCollar(String collar) {
+    this.collar = collar;
+  }
+
+
+  public String getDecration() {
+    return decration;
+  }
+
+  public void setDecration(String decration) {
+    this.decration = decration;
+  }
+
+
+  public String getFabricType() {
+    return fabricType;
+  }
+
+  public void setFabricType(String fabricType) {
+    this.fabricType = fabricType;
+  }
+
+
+  public String getFabricWeight() {
+    return fabricWeight;
+  }
+
+  public void setFabricWeight(String fabricWeight) {
+    this.fabricWeight = fabricWeight;
+  }
+
+
+  public String getFeature() {
+    return feature;
+  }
+
+  public void setFeature(String feature) {
+    this.feature = feature;
+  }
+
+
+  public String getMaterial() {
+    return material;
+  }
+
+  public void setMaterial(String material) {
+    this.material = material;
+  }
+
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+  public String getPatternType() {
+    return patternType;
+  }
+
+  public void setPatternType(String patternType) {
+    this.patternType = patternType;
+  }
+
+
+  public String getProducePlace() {
+    return producePlace;
+  }
+
+  public void setProducePlace(String producePlace) {
+    this.producePlace = producePlace;
+  }
+
+
+  public String getSleeveLength() {
+    return sleeveLength;
+  }
+
+  public void setSleeveLength(String sleeveLength) {
+    this.sleeveLength = sleeveLength;
+  }
+
+
+  public String getSleeveStyle() {
+    return sleeveStyle;
+  }
+
+  public void setSleeveStyle(String sleeveStyle) {
+    this.sleeveStyle = sleeveStyle;
+  }
+
+
+  public String getFengge() {
+    return fengge;
+  }
+
+  public void setFengge(String fengge) {
+    this.fengge = fengge;
+  }
+
+
+  public String getCraft() {
+    return craft;
+  }
+
+  public void setCraft(String craft) {
+    this.craft = craft;
+  }
+
+
+  public String getCustomizeFeature() {
+    return customizeFeature;
+  }
+
+  public void setCustomizeFeature(String customizeFeature) {
+    this.customizeFeature = customizeFeature;
+  }
+
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+
+}
